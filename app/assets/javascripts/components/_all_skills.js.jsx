e31529f5a1a5eef9ef58ebf5a7f145babef5c4fb
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-10-13 15:23:53 -0600

    delete comments about proper binding of React methods and props

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -2,8 +2,6 @@ var AllSkills = React.createClass({
   handleDelete(id) {
     this.props.handleDelete(id)
   },
-  //need to make this method, otherwise we get an error warning that
-  //React component methods may only be bound to the component instance.
 
   render() {
     var skills = this.props.skills.map((skill) => {
@@ -13,8 +11,6 @@ var AllSkills = React.createClass({
           <p><strong> Level:</strong> { skill.level } </p>
           <p> { skill.details } </p>
           <button onClick= { this.handleDelete.bind(this, skill.id) }>Delete</button>
-          // <button onClick= { this.props.handleDelete.bind(this, skill.id) }>Delete</button>
-          // if we do this, we get the error because we are binding to ...?
         </div>
       )
     });
